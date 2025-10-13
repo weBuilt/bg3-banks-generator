ebe2c1bca7e693d750c0cbb0e7e2f37be1678d1a
Aleksandr Senchenko <awb.senchenko@gmail.com>
2025-10-13 13:12:51 +0300

    moved WindowState to Config. renamed WindowState to WindowConfiguration

--- src/main/scala/app/Config.scala
+++ src/main/scala/app/Config.scala
@@ -5,4 +5,25 @@ import java.nio.file.{Path, Paths}
 object Config {
   val configDirectory: Path = Paths.get(System.getProperty("user.home")).resolve(".wblt_bg3bg")
   val windowConfig: Path = configDirectory.resolve("window.json")
+  val appConfig: Path = configDirectory.resolve("app.json")
+
+  case class WindowConfiguration(
+    x: Double = 0d,
+    y: Double = 0d,
+    width: Double = 800d,
+    height: Double = 600d,
+    screen: Int = 0,
+    maximized: Boolean = false,
+  )
+
+  case class AppConfiguration(
+    lastProject: Reference,
+    recent: List[Reference],
+  )
+
+  case class Reference(
+    id: String,
+    name: String,
+  )
+
 }
